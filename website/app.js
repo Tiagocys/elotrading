6dd2021142f4d3ab58a0aa5f0b60391a9e65e6c5
Tiagocys <95282964+Tiagocys@users.noreply.github.com>
2024-06-03 19:32:06 -0300

    Delete website directory

--- website/app.js
+++
@@ -1,9 +0,0 @@
-var nome = document.getElementById('nome');
-var email = document.getElementById('email');
-var conta = document.getElementById('conta');
-
-window.onload = function(){
-    nome.value = '';
-    email.value = '';
-    conta.value = '';
-}
